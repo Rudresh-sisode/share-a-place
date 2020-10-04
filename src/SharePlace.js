3e23f9250a3be9931e0c7fd130b5b77641a9f1fc
Sohame Rajput <rudranrajput@gmail.com>
2020-10-04 16:55:02 +0530

    this programs has updated

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,3 +1,4 @@
+import {Modal} from './UI/Model';
 class PlaceFinder{
     constructor(){
         const addressForm = document.querySelector('form');
@@ -13,13 +14,17 @@ class PlaceFinder{
             alert("location feature is not available in your browser\nplease use modern browser");
             return;
         }
+        const model= new Modal('loading-modal-content','Loading location - Please wait');
+        model.show();//loading-modal-content
         navigator.geolocation.getCurrentPosition(successResult => {
+            model.hide();
             const coordinates ={
                 lat:successResult.coords.latitude ,
                 lng:successResult.coords.longitude
             };
             console.log('my coordinate ', coordinates);
         }, error =>{
+            model.hide();
             alert("couldn't locate your unfortunately. please come after one");
         });
     }
--- src/UI/Model.js
+++ src/UI/Model.js
@@ -1,28 +1,41 @@
-class Model{
-    constructor(contentId, fallbackCode){
-        this.fallbackCode=fallbackCode;
-        this.contentTemplateEl = document.getElementById(contentId);
-        this.modelTemplateEl = document.getElementById('modal-template');
+export class Modal {
+    constructor(contentId, fallbackText) {
+      this.fallbackText = fallbackText;
+      this.contentTemplateEl = document.getElementById(contentId);
+      this.modalTemplateEl = document.getElementById('modal-template');
     }
-
-    show(){
-        //mdn template
-        //to check if browser compatiable with template or not
-        if('content' in document.createElement('template')){
-            //here we are accessing following code
-            /*
-                <div class="backdrop"></div>
-      <div class="modal"></div>
-            */
-            const modalElements = document.importNode(this.modelTemplateEl, true);
+  
+    show() {
+      if ('content' in document.createElement('template')) {
+        const modalElements = document.importNode(
+          this.modalTemplateEl.content,
+          true
+        );
+        this.modalElement = modalElements.querySelector('.modal');
+        this.backdropElement = modalElements.querySelector('.backdrop');
+        const contentElement = document.importNode(
+          this.contentTemplateEl.content,
+          true
+        );
+  
+        this.modalElement.appendChild(contentElement);
+  
+        document.body.insertAdjacentElement('afterbegin', this.modalElement);
+        document.body.insertAdjacentElement('afterbegin', this.backdropElement);
+      } else {
+        // fallback code
+        alert(this.fallbackText);
+      }
+    }
+  
+    hide() {
+        if(this.modalElement){
+            document.body.removeChild(this.modalElement);
+            document.body.removeChild(this.backdropElement);
+            this.modalElement = null;
+            this.backdropElement = null;
         }
-        else{
-            //fallback code
-            alert(this.fallbackCode);
-        }
-    }
-
-    hide(){
 
     }
-}
+  }
+  
